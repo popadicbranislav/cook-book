121c8724806c15c1f09828efab24ca2e450ed645
Branislav Popadič <popadicbranislav@gmail.com>
2023-07-21 00:30:49 +0200

    style: style update for the search bar and the header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,22 @@
 <template>
-  <header class="y sticky top-0 z-9 bg-#333 text-white">
-    <div class="mx-auto px-2 py-3 container">
+  <header class="sticky top-0 z-9 bg-#333 text-white">
+    <div
+      class="mx-auto flex items-center gap-4 p-4 container"
+    >
       <NuxtLink to="/">
         <h1 class="text-xl uppercase">
           Cook book
         </h1>
+      </NuxtLink>
+
+      <NuxtLink to="#" class="ml-auto hover:(decoration-underline decoration-dashed)">
+        About
+      </NuxtLink>
+      <NuxtLink to="#" class="hover:(decoration-underline decoration-dashed)">
+        Create
+      </NuxtLink>
+      <NuxtLink to="#" class="ml-xl hover:(decoration-underline decoration-dashed)">
+        Login
       </NuxtLink>
     </div>
   </header>
--- pages/index.vue
+++ pages/index.vue
@@ -21,19 +21,18 @@ const { data: testData } = useAsyncData('test', async () => {
 <template>
   <AppHeader />
 
-  <main class="py-8">
-    {{ testData }}
-    <ul class="mx-auto mb-20 flex flex-col items-center gap-8 px-2 container">
+  <main class="mx-auto py-8 container">
+    <ul>
       <li
         v-for="recipe of recipes" :key="recipe.title"
-        class="max-w-40rem w-full"
+        class="mx-auto mb-4 max-w-xl"
       >
         <NuxtLink
           :to="`/recipes/${recipe.id}`"
           tabindex="0"
         >
           <div
-            class="relative aspect-3/2 flex flex-col justify-end of-hidden from-white/20 to-black/50 bg-gradient-to-b p-8 hover:from-white/5 hover:to-black/10"
+            class="relative aspect-3/2 flex flex-col justify-end of-hidden from-white/20 to-black/50 bg-gradient-to-b p-8 hover:(from-white/5 to-black/10)"
           >
             <img
               :src="recipe.img_url" class="absolute inset-0 z--1"
@@ -52,19 +51,20 @@ const { data: testData } = useAsyncData('test', async () => {
       </li>
     </ul>
 
-    <div class="fixed bottom-10 left-0 right-0 mx-auto px-4 container">
+    <div class="sticky bottom-4 my-50vh">
       <form
-        class="flex items-center bg-gray-100 pr-5 text-gray-800 transition-shadow focus-within:(shadow-2xl outline)"
+        class="m-4 flex items-center of-hidden rounded-xl bg-white shadow-2xl shadow-gray-8 focus-within:(ring-4)"
         @submit.prevent
       >
         <input
           v-model="searchQuery"
-          class="flex-grow-1 bg-transparent px-6 py-3 focus:outline-none"
           type="search"
           placeholder="Search recipes..."
+          class="flex-1 bg-transparent p-4 outline-none"
         >
-        <button type="submit" @click="handleSubmit">
-          <div class="i-carbon-search text-xs" />
+
+        <button type="submit" class="px-2 text-1em outline-none" @click="handleSubmit">
+          <div class="i-carbon-search" />
         </button>
       </form>
     </div>
